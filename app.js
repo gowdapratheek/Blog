82ec89572c52ecc95cd7b4bd26bccd60cddacc74
H G Pratheek Gowda <93853432+gowdapratheek@users.noreply.github.com>
2023-10-01 19:58:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import  static as expressStatic  from "express";
+import  expressStatic  from "static";
 import bodyParser from "body-parser";
 import lodash from "lodash";
 
